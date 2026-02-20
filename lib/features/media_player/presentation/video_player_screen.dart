8721f771ef8ba35892cae9210a672118637ab453
BajWarring <91818323+BajWarring@users.noreply.github.com>
2026-02-20 23:05:19 +0530

    Update video_player_screen.dart

--- lib/features/media_player/presentation/video_player_screen.dart
+++ lib/features/media_player/presentation/video_player_screen.dart
@@ -1,6 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:media_kit/media_kit.dart';
-import 'package:media_kit_video/media_kit_video.dart';
+import 'package:video_player/video_player.dart';
 import '../../../../filesystem/domain/entities/omni_node.dart';
 
 class VideoPlayerScreen extends StatefulWidget {
@@ -12,20 +12,22 @@ class VideoPlayerScreen extends StatefulWidget {
 }
 
 class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
-  late final Player player;
-  late final VideoController controller;
+  late VideoPlayerController _controller;
 
   @override
   void initState() {
     super.initState();
-    player = Player();
-    controller = VideoController(player);
-    player.open(Media(widget.videoNode.path));
+    _controller = VideoPlayerController.file(File(widget.videoNode.path))
+      ..initialize().then((_) {
+        // Ensure the first frame is shown and play automatically
+        setState(() {});
+        _controller.play();
+      });
   }
 
   @override
   void dispose() {
-    player.dispose();
+    _controller.dispose();
     super.dispose();
   }
 
@@ -33,8 +35,29 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      appBar: AppBar(backgroundColor: Colors.transparent, iconTheme: const IconThemeData(color: Colors.white)),
-      body: Center(child: Video(controller: controller)),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent, 
+        iconTheme: const IconThemeData(color: Colors.white),
+        elevation: 0,
+      ),
+      body: Center(
+        child: _controller.value.isInitialized
+            ? AspectRatio(
+                aspectRatio: _controller.value.aspectRatio,
+                child: VideoPlayer(_controller),
+              )
+            : const CircularProgressIndicator(color: Colors.white),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _controller.value.isPlaying ? _controller.pause() : _controller.play();
+          });
+        },
+        child: Icon(
+          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+        ),
+      ),
     );
   }
 }
