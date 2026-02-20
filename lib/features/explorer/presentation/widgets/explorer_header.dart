a28fdbd02888712d8f3e28da57483682466d6833
BajWarring <91818323+BajWarring@users.noreply.github.com>
2026-02-20 20:44:40 +0530

    Update explorer_header.dart

--- lib/features/explorer/presentation/widgets/explorer_header.dart
+++ lib/features/explorer/presentation/widgets/explorer_header.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../../filesystem/application/directory_notifier.dart';
+import '../../../../filesystem/application/directory_notifier.dart';
+import '../../../search/presentation/search_screen.dart';
 
 class ExplorerHeader extends ConsumerWidget implements PreferredSizeWidget {
   final bool isSelectionMode;
