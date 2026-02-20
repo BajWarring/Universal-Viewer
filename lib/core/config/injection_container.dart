801e1de660b42bf9f32b055785ca39fb6ec65589
BajWarring <91818323+BajWarring@users.noreply.github.com>
2026-02-20 20:41:38 +0530

    Update and rename injection_conatiner.dart to injection_container.dart

---
+++ lib/core/config/injection_container.dart
@@ -0,0 +1,12 @@
+import 'package:get_it/get_it.dart';
+import '../../filesystem/domain/repositories/file_system_provider.dart';
+import '../../filesystem/data/providers/local_file_system_provider.dart';
+
+final sl = GetIt.instance;
+
+Future<void> initializeDependencies() async {
+  sl.registerLazySingleton<FileSystemProvider>(
+    () => LocalFileSystemProvider(),
+    instanceName: 'local'
+  );
+}
