c1461b96caedfc8c844bb2a33123ad29c00f4a68
BajWarring <91818323+BajWarring@users.noreply.github.com>
2026-02-21 09:26:28 +0530

    Update local_file_system_provider.dart

--- lib/filesystem/data/providers/local_file_system_provider.dart
+++ lib/filesystem/data/providers/local_file_system_provider.dart
@@ -4,6 +4,16 @@ import '../../domain/entities/omni_node.dart';
 import '../../domain/repositories/file_system_provider.dart';
 
 class LocalFileSystemProvider implements FileSystemProvider {
+  
+  @override
+  Future<List<OmniNode>> getRoots() async {
+    // Return standard Android storage roots
+    return [
+      OmniFolder(name: 'Internal Storage', path: '/storage/emulated/0', modified: DateTime.now()),
+      OmniFolder(name: 'SD Card', path: '/storage/sdcard1', modified: DateTime.now()),
+    ];
+  }
+
   @override
   Future<List<OmniNode>> listDirectory(String path) async {
     final dir = Directory(path);
@@ -16,7 +26,7 @@ class LocalFileSystemProvider implements FileSystemProvider {
       final stat = await entity.stat();
       final name = p.basename(entity.path);
       
-      // Skip hidden files for now (files starting with a dot)
+      // Skip hidden files for now
       if (name.startsWith('.')) continue;
 
       if (entity is Directory) {
@@ -45,5 +55,48 @@ class LocalFileSystemProvider implements FileSystemProvider {
     });
 
     return nodes;
+  }
+
+  @override
+  Future<OmniNode> createFolder(String parentPath, String folderName) async {
+    final newDir = Directory(p.join(parentPath, folderName));
+    await newDir.create();
+    final stat = await newDir.stat();
+    return OmniFolder(name: folderName, path: newDir.path, modified: stat.modified);
+  }
+
+  @override
+  Future<bool> delete(String path) async {
+    final entity = FileSystemEntity.typeSync(path) == FileSystemEntityType.directory 
+        ? Directory(path) 
+        : File(path);
+    if (await entity.exists()) {
+      await entity.delete(recursive: true);
+      return true;
+    }
+    return false;
+  }
+
+  @override
+  Future<OmniNode> rename(String path, String newName) async {
+    final entity = FileSystemEntity.typeSync(path) == FileSystemEntityType.directory 
+        ? Directory(path) 
+        : File(path);
+        
+    final newPath = p.join(p.dirname(path), newName);
+    final renamedEntity = await entity.rename(newPath);
+    final stat = await renamedEntity.stat();
+
+    if (renamedEntity is Directory) {
+      return OmniFolder(name: newName, path: newPath, modified: stat.modified);
+    } else {
+      return OmniFile(
+        name: newName, 
+        path: newPath, 
+        size: stat.size, 
+        modified: stat.modified, 
+        extension: p.extension(newPath).replaceAll('.', '')
+      );
+    }
   }
 }
