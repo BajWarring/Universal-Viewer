a1826faa729a72a36a7d07074c8f1adc100ada6b
BajWarring <91818323+BajWarring@users.noreply.github.com>
2026-02-21 09:20:03 +0530

    Update archive_service.dart

--- lib/features/archive_engine/application/archive_service.dart
+++ lib/features/archive_engine/application/archive_service.dart
@@ -1,58 +1,37 @@
 import 'dart:io';
-import 'dart:isolate';
 import 'package:archive/archive_io.dart';
-import 'package:path/path.dart' as path_utils;
-
-class CompressParams {
-  final String sourcePath;
-  final String destPath;
-  final String format;
-  final String? password;
-
-  CompressParams(this.sourcePath, this.destPath, this.format, this.password);
-}
+import 'package:path/path.dart' as p;
 
 class ArchiveService {
-  
-  /// Runs compression in a separate background isolate
-  static Future<void> compressDirectory(CompressParams params) async {
-    await Isolate.run(() async {
-      final encoder = ZipFileEncoder();
-      
-      // The archive package supports password protection for ZIPs
-      if (params.password != null && params.password!.isNotEmpty) {
-        encoder.zipDirectory(
-          Directory(params.sourcePath),
-          filename: params.destPath,
-          // Note: Full AES encryption requires specific archive package implementations 
-          // or a native channel fallback for 7z/RAR.
-        );
+  Future<void> compress(List<String> filePaths, String destinationZip) async {
+    var encoder = ZipFileEncoder();
+    encoder.create(destinationZip);
+    
+    for (String path in filePaths) {
+      final stat = await FileStat.stat(path);
+      if (stat.type == FileSystemEntityType.directory) {
+        encoder.addDirectory(Directory(path));
       } else {
-        encoder.zipDirectory(
-          Directory(params.sourcePath),
-          filename: params.destPath,
-        );
+        encoder.addFile(File(path));
       }
-    });
+    }
+    encoder.close();
   }
 
-  /// Extracts an archive in a background isolate
-  static Future<void> extractArchive(String archivePath, String destDirPath) async {
-    await Isolate.run(() async {
-      final bytes = File(archivePath).readAsBytesSync();
-      final archive = ZipDecoder().decodeBytes(bytes);
-
-      for (final file in archive) {
-        final filename = file.name;
-        if (file.isFile) {
-          final data = file.content as List<int>;
-          final outFile = File(path_utils.join(destDirPath, filename));
-          outFile.createSync(recursive: true);
-          outFile.writeAsBytesSync(data);
-        } else {
-          Directory(path_utils.join(destDirPath, filename)).createSync(recursive: true);
-        }
+  Future<void> extract(String zipPath, String destinationDir) async {
+    final bytes = await File(zipPath).readAsBytes();
+    final archive = ZipDecoder().decodeBytes(bytes);
+    
+    for (final file in archive) {
+      final filename = file.name;
+      if (file.isFile) {
+        final data = file.content as List<int>;
+        File(p.join(destinationDir, filename))
+          ..createSync(recursive: true)
+          ..writeAsBytesSync(data);
+      } else {
+        Directory(p.join(destinationDir, filename)).createSync(recursive: true);
       }
-    });
+    }
   }
 }
