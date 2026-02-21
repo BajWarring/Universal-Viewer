b9da6501b90a9edd403ac7b16351f5485ca2b0fb
BajWarring <91818323+BajWarring@users.noreply.github.com>
2026-02-21 09:25:55 +0530

    Update file_system_provider.dart

--- lib/filesystem/domain/repositories/file_system_provider.dart
+++ lib/filesystem/domain/repositories/file_system_provider.dart
@@ -1,13 +1,9 @@
 import '../entities/omni_node.dart';
 
 abstract class FileSystemProvider {
+  Future<List<OmniNode>> getRoots();
   Future<List<OmniNode>> listDirectory(String path);
-
-  /// File Operations
-  Future<void> createFolder(String path, String folderName);
-  Future<void> rename(String path, String newName);
-  Future<void> delete(String path);
-  
-  /// Resolves the root paths (e.g., Internal Storage, SD Card)
-  Future<List<OmniFolder>> getRoots();
+  Future<OmniNode> createFolder(String parentPath, String folderName);
+  Future<bool> delete(String path);
+  Future<OmniNode> rename(String path, String newName);
 }
