aec28923a03b8a43633db0f26d23f50f67c5d745
BajWarring <91818323+BajWarring@users.noreply.github.com>
2026-02-21 09:10:11 +0530

    Update audio_notifier.dart

--- lib/features/media_player/application/audio_notifier.dart
+++ lib/features/media_player/application/audio_notifier.dart
@@ -4,7 +4,25 @@ import '../../../../filesystem/domain/entities/omni_node.dart';
 
 class AudioState {
   final OmniNode? currentTrack;
-  const AudioState({this.currentTrack});
+  final bool isPlaying;
+  final Duration position;
+  final Duration duration;
+
+  const AudioState({
+    this.currentTrack,
+    this.isPlaying = false,
+    this.position = Duration.zero,
+    this.duration = Duration.zero,
+  });
+
+  AudioState copyWith({OmniNode? track, bool? isPlaying, Duration? pos, Duration? dur}) {
+    return AudioState(
+      currentTrack: track ?? currentTrack,
+      isPlaying: isPlaying ?? this.isPlaying,
+      position: pos ?? position,
+      duration: dur ?? duration,
+    );
+  }
 }
 
 class AudioNotifier extends Notifier<AudioState> {
@@ -13,13 +31,34 @@ class AudioNotifier extends Notifier<AudioState> {
   @override
   AudioState build() {
     _player = AudioPlayer();
+    
+    // Listen to player streams to update UI state
+    _player.positionStream.listen((p) => state = state.copyWith(pos: p));
+    _player.durationStream.listen((d) => state = state.copyWith(dur: d ?? Duration.zero));
+    _player.playingStream.listen((p) => state = state.copyWith(isPlaying: p));
+    
     return const AudioState();
   }
 
   Future<void> playFile(OmniNode node) async {
-    state = AudioState(currentTrack: node);
+    state = state.copyWith(track: node);
     await _player.setFilePath(node.path);
     _player.play();
+  }
+
+  void togglePlayPause() {
+    if (_player.playing) {
+      _player.pause();
+    } else {
+      _player.play();
+    }
+  }
+
+  void seek(Duration position) => _player.seek(position);
+
+  void stopAndDismiss() {
+    _player.stop();
+    state = const AudioState(); // Clears current track, hiding the mini-player
   }
 }
 
