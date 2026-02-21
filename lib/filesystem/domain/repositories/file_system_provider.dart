768a0b28e4c66d17be71017a855277d9b967c80e
BajWarring <91818323+BajWarring@users.noreply.github.com>
2026-02-21 09:19:11 +0530

    Update file_system_provider.dart

--- lib/filesystem/domain/repositories/file_system_provider.dart
+++ lib/filesystem/domain/repositories/file_system_provider.dart
@@ -1,12 +1,8 @@
-import 'package:omni_file_manager/filesystem/domain/entities/omni_node.dart';
+import '../entities/omni_node.dart';
 
 abstract class FileSystemProvider {
-  String get providerId;
-  String get displayName;
-  
-  /// Fetches the contents of a specific directory path
   Future<List<OmniNode>> listDirectory(String path);
-  
+
   /// File Operations
   Future<void> createFolder(String path, String folderName);
   Future<void> rename(String path, String newName);
